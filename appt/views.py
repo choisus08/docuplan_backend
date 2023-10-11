36ba5d05c3365411d623b0da9dd7f6da0410db4a
Susie Gordon <susieyoungchoi@gmail.com>
2023-10-10 20:33:23 -0400

    change views

--- appt/views.py
+++ appt/views.py
@@ -5,7 +5,7 @@ from .serializers import AppointmentSerializer;
 # Create your views here.
 class AppointmentViewSet(viewsets.ModelViewSet):
     # queryset is a list of all Appointment objects
-    queryset = Appointment.objects.all()
+    queryset = Appointment.objects.all().order_by('id')
     # The serializer_class attribute is used to control which serializer class should be used for serializing and deserializing queryset and model instances.
     serializer_class = AppointmentSerializer
     # Set permission_classes to allow unrestricted access to the API.
